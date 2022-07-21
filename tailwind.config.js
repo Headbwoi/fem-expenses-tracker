89e3c36079bc5dcf244215c2c05ff2ba5285237d
headbwoi <tochukwujohn24@gmail.com>
2022-07-21 09:05:29 +0100

    finished config

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "",
+    },
+    colors: {
+      
+    },
+    fontFamily: {
+      sans: ["'jay', 'sans-serif'"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
